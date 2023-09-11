de3524c8e50fbe02ac588c0526774731379d5e4b
Marcel <91682010+wintermuteXX@users.noreply.github.com>
2023-09-11 12:09:54 +0000

    prettier

--- src/prototypes/room.ts
+++ src/prototypes/room.ts
@@ -30,68 +30,78 @@ Room.prototype.getMineral = function () {
   return this._mineral;
 };
 
-Room.prototype.hostiles = function() {
-		if (!this._hostiles) {
-			this._hostiles = this.find(FIND_HOSTILE_CREEPS);
-		}
-		return this._hostiles;
-}
+Room.prototype.hostiles = function () {
+  if (!this._hostiles) {
+    this._hostiles = this.find(FIND_HOSTILE_CREEPS);
+  }
+  return this._hostiles;
+};
 
-Room.prototype.invaders = function() {
-		if (!this._invaders) {
-			this._invaders = _.filter(this.hostiles, (creep: Creep) => creep.owner.username == 'Invader');
-		}
-		return this._invaders;
-}
+Room.prototype.invaders = function () {
+  if (!this._invaders) {
+    this._invaders = _.filter(this.hostiles, (creep: Creep) => creep.owner.username == 'Invader');
+  }
+  return this._invaders;
+};
 
-Room.prototype.sourceKeepers = function() {
-		if (!this._sourceKeepers) {
-			this._sourceKeepers = _.filter(this.hostiles, (creep: Creep) => creep.owner.username == 'Source Keeper');
-		}
-		return this._sourceKeepers;
-}
+Room.prototype.sourceKeepers = function () {
+  if (!this._sourceKeepers) {
+    this._sourceKeepers = _.filter(this.hostiles, (creep: Creep) => creep.owner.username == 'Source Keeper');
+  }
+  return this._sourceKeepers;
+};
 
-Room.prototype.playerHostiles = function() {
-		if (!this._playerHostiles) {
-			this._playerHostiles = _.filter(this.hostiles,
-											(creep: Creep) => creep.owner.username != 'Invader'
-															  && creep.owner.username != 'Source Keeper');
-		}
-		return this._playerHostiles;
-}
+Room.prototype.playerHostiles = function () {
+  if (!this._playerHostiles) {
+    this._playerHostiles = _.filter(
+      this.hostiles,
+      (creep: Creep) => creep.owner.username != 'Invader' && creep.owner.username != 'Source Keeper'
+    );
+  }
+  return this._playerHostiles;
+};
 
-Room.prototype.dangerousHostiles = function() {
-		if (!this._dangerousHostiles) {
-			if (this.my) {
-				this._dangerousHostiles = _.filter(this.hostiles,
-												   (creep: Creep) => creep.getActiveBodyparts(ATTACK) > 0
-																	 || creep.getActiveBodyparts(WORK) > 0
-																	 || creep.getActiveBodyparts(RANGED_ATTACK) > 0
-																	 || creep.getActiveBodyparts(HEAL) > 0);
-			} else {
-				this._dangerousHostiles = _.filter(this.hostiles,
-												   (creep: Creep) => creep.getActiveBodyparts(ATTACK) > 0
-																	 || creep.getActiveBodyparts(RANGED_ATTACK) > 0
-																	 || creep.getActiveBodyparts(HEAL) > 0);
-			}
-		}
-		return this._dangerousHostiles;
-}
+Room.prototype.dangerousHostiles = function () {
+  if (!this._dangerousHostiles) {
+    if (this.my) {
+      this._dangerousHostiles = _.filter(
+        this.hostiles,
+        (creep: Creep) =>
+          creep.getActiveBodyparts(ATTACK) > 0 ||
+          creep.getActiveBodyparts(WORK) > 0 ||
+          creep.getActiveBodyparts(RANGED_ATTACK) > 0 ||
+          creep.getActiveBodyparts(HEAL) > 0
+      );
+    } else {
+      this._dangerousHostiles = _.filter(
+        this.hostiles,
+        (creep: Creep) =>
+          creep.getActiveBodyparts(ATTACK) > 0 ||
+          creep.getActiveBodyparts(RANGED_ATTACK) > 0 ||
+          creep.getActiveBodyparts(HEAL) > 0
+      );
+    }
+  }
+  return this._dangerousHostiles;
+};
 
-Room.prototype.dangerousPlayerHostiles = function() {
-		if (!this._dangerousPlayerHostiles) {
-			this._dangerousPlayerHostiles = _.filter(this.playerHostiles,
-													 (c: Creep) => c.getActiveBodyparts(ATTACK) > 0
-																   || c.getActiveBodyparts(WORK) > 0
-																   || c.getActiveBodyparts(RANGED_ATTACK) > 0
-																   || c.getActiveBodyparts(HEAL) > 0);
-		}
-		return this._dangerousPlayerHostiles;
-}
+Room.prototype.dangerousPlayerHostiles = function () {
+  if (!this._dangerousPlayerHostiles) {
+    this._dangerousPlayerHostiles = _.filter(
+      this.playerHostiles,
+      (c: Creep) =>
+        c.getActiveBodyparts(ATTACK) > 0 ||
+        c.getActiveBodyparts(WORK) > 0 ||
+        c.getActiveBodyparts(RANGED_ATTACK) > 0 ||
+        c.getActiveBodyparts(HEAL) > 0
+    );
+  }
+  return this._dangerousPlayerHostiles;
+};
 
 Room.prototype.toString = function (htmlLink = true) {
-	if (htmlLink) {
-		return `<a href="#!/room/${Game.shard.name}/${this.name}">[${this.name}]</a>`;
-	}
-	return `[(${this.name}) #${this.name}]`;
+  if (htmlLink) {
+    return `<a href="#!/room/${Game.shard.name}/${this.name}">[${this.name}]</a>`;
+  }
+  return `[(${this.name}) #${this.name}]`;
 };
--- src/prototypes/roomPosition.ts
+++ src/prototypes/roomPosition.ts
@@ -2,27 +2,27 @@
  * Room Position prototypes
  */
 
-RoomPosition.prototype.countFreePositions = function() {
-	let freeSpaceCount: number = 0;
+RoomPosition.prototype.countFreePositions = function () {
+  let freeSpaceCount: number = 0;
   let terrain = new Room.Terrain(this.roomName);
-	[this.x - 1, this.x, this.x + 1].forEach((x) => {
-		[this.y - 1, this.y, this.y + 1].forEach((y) => {
-			if (!(x == this.x && this.y == y)) {
-				if (terrain.get(this.x, this.y) !== TERRAIN_MASK_WALL) freeSpaceCount++;
-			}
-		}, this);
-	}, this);
-	return freeSpaceCount;
+  [this.x - 1, this.x, this.x + 1].forEach(x => {
+    [this.y - 1, this.y, this.y + 1].forEach(y => {
+      if (!(x == this.x && this.y == y)) {
+        if (terrain.get(this.x, this.y) !== TERRAIN_MASK_WALL) freeSpaceCount++;
+      }
+    }, this);
+  }, this);
+  return freeSpaceCount;
 };
 
 RoomPosition.prototype.toString = function (htmlLink = true, id = undefined) {
-	if (htmlLink) {
-		var onClick = "";
-		if (id)
-			onClick +=
-				`angular.element('body').injector().get('RoomViewPendingSelector').set('${id}');` +
-				`angular.element($('body')).scope().$broadcast('roomObjectSelected', _.filter(angular.element(document.getElementsByClassName('room ng-scope')).scope().Room.objects, (o)=>o._id==='${id}')[0]);`;
-		return `<a href="#!/room/${Game.shard.name}/${this.roomName}" onClick="${onClick}">[${this.roomName} ${this.x},${this.y}]</a>`;
-	}
-	return `[${this.roomName} ${this.x},${this.y}]`;
+  if (htmlLink) {
+    var onClick = '';
+    if (id)
+      onClick +=
+        `angular.element('body').injector().get('RoomViewPendingSelector').set('${id}');` +
+        `angular.element($('body')).scope().$broadcast('roomObjectSelected', _.filter(angular.element(document.getElementsByClassName('room ng-scope')).scope().Room.objects, (o)=>o._id==='${id}')[0]);`;
+    return `<a href="#!/room/${Game.shard.name}/${this.roomName}" onClick="${onClick}">[${this.roomName} ${this.x},${this.y}]</a>`;
+  }
+  return `[${this.roomName} ${this.x},${this.y}]`;
 };
